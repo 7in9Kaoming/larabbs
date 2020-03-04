96e51ce01123bedccd98ac4b4a5e922031ad4e5b
lingkaoming <lingkaoming@outlook.com>
2020-03-04 05:50:43 +0000

    分类话题列表

--- app/helpers.php
+++ app/helpers.php
@@ -4,3 +4,9 @@ function route_class()
 {
 	return str_replace('.', '-', Route::currentRouteName());
 }
+
+// 设置导航的active状态
+function category_nav_active($category_id)
+{
+    return active_class((if_route('categories.show') && if_route_param('category', $category_id)));
+}
