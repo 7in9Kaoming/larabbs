d63ad4967daca0f0b73573451262f839f848d4fe
lingkaoming <lingkaoming@outlook.com>
2020-03-05 04:10:27 +0000

    引入Simditor编辑器

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,30 +1,31 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!-- CSRF Token -->
-	<meta name="csrf-token" content="{{ csrf_token() }}">
-	
-	<title>@yield('title', 'LaraBBS')</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-	<link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
+  <title>@yield('title', 'LaraBBS')</title>
+  <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
+  @yield('SimditorStyle')
 </head>
 <body>
-	<div id="app" class="{{ route_class() }}-page">
-		@include('layouts._header')
+  <div id="app" class="{{ route_class() }}-page">
+    @include('layouts._header')
 
-		<div class="container">
-			@include('shared._messages')
+    <div class="container">
+      @include('shared._messages')
 
-			@yield('content')
-		</div>
+      @yield('content')
+    </div>
 
-		@include('layouts._footer')
-	</div>
+    @include('layouts._footer')
+  </div>
 
-	<!-- Scripts -->
-	<script src="{{ mix('js/app.js') }}"></script>
+  <!-- Scripts -->
+  <script src="{{ mix('js/app.js') }}"></script>
+  @yield('SimditorScript')
 </body>
 </html>
