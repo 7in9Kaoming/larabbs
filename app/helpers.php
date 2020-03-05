a66c03d9b1381bc2971d445fc2bf889e5e4a0b80
lingkaoming <lingkaoming@outlook.com>
2020-03-05 03:36:28 +0000

    用户可以创建话题

--- app/helpers.php
+++ app/helpers.php
@@ -10,3 +10,15 @@ function category_nav_active($category_id)
 {
     return active_class((if_route('categories.show') && if_route_param('category', $category_id)));
 }
+
+/**
+ * 生成文章摘要
+ * @param  string  $value  文章的内容
+ * @param  integer $length 摘要的长度
+ * @return string          生成的摘要
+ */
+function make_excerpt($value, $length = 200)
+{
+    $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
+    return Str::limit($excerpt, $length);
+}
