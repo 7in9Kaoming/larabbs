099c734f215f055a9d2f38e780ba694c679af5d3
lingkaoming <lingkaoming@outlook.com>
2020-03-06 03:31:54 +0000

    引入百度翻译标题生成slug

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,27 +3,23 @@
 namespace App\Observers;
 
 use App\Models\Topic;
+use App\Handler\SlugTranslateHandler;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
 class TopicObserver
 {
-    public function creating(Topic $topic)
-    {
-
-    }
-
     public function saving(Topic $topic)
     {
         // 过滤文章内容中的不安全标签，防止XSS攻击
         $topic->body = clean($topic->body, 'user_topic_body');
 
         $topic->excerpt = make_excerpt($topic->body);
-    }
 
-    public function updating(Topic $topic)
-    {
-
+        // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
+        if ( ! $topic->slug) {
+            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+        }
     }
 }
