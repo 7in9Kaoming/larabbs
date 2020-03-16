65c57a86933dc1737e9c39a1737420c2cb11ca97
lingkaoming <lingkaoming@outlook.com>
2020-03-16 04:04:53 +0000

    站点设置

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer">
   <div class="container">
 
-    <p class="float-right"><a href="#">联系我们</a></p>
+    <p class="float-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
   </div>
 </footer>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
-
-  <title>@yield('title', 'LaraBBS')</title>
+  <title>@yield('title', 'LaraBBS') - {{ setting('site_name', 'Laravel') }}</title>
+  <meta name="description" content="@yield('description', setting('seo_description', 'LaraBBS 爱好者社区。'))" />
+  <meta name="keyword" content="@yield('keyword', setting('seo_keyword', 'LaraBBS,社区,论坛,开发者论坛'))" />
   <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
   @yield('SimditorStyle')
 </head>
